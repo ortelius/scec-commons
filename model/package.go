ae2203280b61e01477eb3c3f0bf8a3c3d3d06c8e
Steve Taylor <steve@deployhub.com>
2024-03-13 11:23:20 -0600

    add PackageLicense and PackageCVE for backward compat Signed-off-by: Steve Taylor <steve@deployhub.com>

--- model/package.go
+++ model/package.go
@@ -18,3 +18,39 @@ type Package struct {
 func NewPackage() *Package {
 	return &Package{}
 }
+
+// NOTE: PackageLicense and PackageCVE are for backward compatibility with v10 frontend
+
+// PackageLicense defines a SBOM package dependency with the corresponding license (subset of the full sbom)
+type PackageLicense struct {
+	Language string  `json:"pkgtype"`
+	License  string  `json:"name"` // name of the license
+	Name     string  `json:"packagename"`
+	Score    float64 `json:"score"`
+	Severity string  `json:"risklevel"`
+	Summary  string  `json:"summary"`
+	URL      string  `json:"url"` // url to the license details
+	Version  string  `json:"packageversion"`
+}
+
+// NewPackageLicense is the contructor that sets the appropriate default values
+func NewPackageLicense() *PackageLicense {
+	return &PackageLicense{}
+}
+
+// PackageCVE defines a SBOM package dependency with the corresponding license (subset of the full sbom)
+type PackageCVE struct {
+	CVE      string  `json:"name"` // CVE name
+	Language string  `json:"pkgtype"`
+	Name     string  `json:"packagename"`
+	Score    float64 `json:"score"`
+	Severity string  `json:"risklevel"`
+	Summary  string  `json:"summary"`
+	URL      string  `json:"url"` // CVE url on osv.dev
+	Version  string  `json:"packageversion"`
+}
+
+// NewPackageCVE is the contructor that sets the appropriate default values
+func NewPackageCVE() *PackageCVE {
+	return &PackageCVE{}
+}
